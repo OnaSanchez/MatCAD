999ea0f73e49427da074bcf9b6b965f9ce069633
Ona Sánchez Núñez <125382161+OnaSanchez@users.noreply.github.com>
2023-05-03 19:55:05 +0200

    Delete Càlcul numèric directory

--- Càlcul numèric/Fractals/nwtfr.h
+++
@@ -1,8 +0,0 @@
-void avalp (double x, double y, double *px, double *py,
-      int n, double u[], double v[]);
-
-void avaldp (double x, double y, double *dpx, double *dpy,
-      int n, double u[], double v[]);
-
-int cnvnwt (double x, double y, double tolcnv, int maxit,
-      int n, double u[], double v[]);
